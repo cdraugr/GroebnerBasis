c942dc13577a499b3afa49c49a794e7f9417fedf
cdraugr- <kvm2014@bk.ru>
2020-06-17 22:22:27 +0300

    new tests to main added

--- TestGroebnerLib/srcs/main.cpp
+++ TestGroebnerLib/srcs/main.cpp
@@ -1,14 +1,16 @@
 #include "Tests.h"
 
 int main() {
-    test_Rational();
-    test_Modular();
-    test_Monomial();
-    test_Term();
-    test_TermOrder();
-    test_Polynomial();
-    test_PolynomialOrder();
-    test_PolynomialSet();
+    // test_Rational();
+    // test_Modular();
+    // test_Monomial();
+    // test_Term();
+    // test_TermOrder();
+    // test_Polynomial();
+    // test_PolynomialOrder();
+    // test_PolynomialSet();
     // test_Sigma(6);
     // test_SigmaTime(6);
+    // test_CriticalPair();
+    test_FastGB();
 }
