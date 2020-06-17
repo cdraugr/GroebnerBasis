ecf927519f9de00c782b8df9d290f5a13c64ca7a
cdraugr- <kvm2014@bk.ru>
2020-06-17 22:12:26 +0300

    F4 added

---
+++ GroebnerLib/includes/FastGB.h
@@ -0,0 +1,145 @@
+#pragma once
+
+#include "MatrixReduction.h"
+#include "CriticalPairs.h"
+
+namespace gb {
+
+/* Declaration */
+
+template <typename T, typename Comp>
+CriticalPairs<T, Comp> normal_select(CriticalPairs<T, Comp>&);
+
+template <typename T, typename Comp>
+static std::pair<PolynomialSet<T, Comp>, typename Polynomial<T, Comp>::container>
+sym_preproc(const CriticalPairs<T, Comp>&, const PolynomialSet<T, Comp>&);
+
+template <typename T, typename Comp>
+static PolynomialSet<T, Comp>
+reduction(const CriticalPairs<T, Comp>&, const PolynomialSet<T, Comp>&);
+
+template <typename T, typename Comp, typename SelFunction = decltype(normal_select<T, Comp>)>
+PolynomialSet<T, Comp>& calculated_fast_gb(PolynomialSet<T, Comp>&, SelFunction = normal_select);
+
+template <typename T, typename Comp, typename SelFunction = decltype(normal_select<T, Comp>)>
+PolynomialSet<T, Comp>
+calculate_fast_gb(const PolynomialSet<T, Comp>&, SelFunction = normal_select);
+
+/* Realization */
+
+template <typename T, typename Comp>
+CriticalPairs<T, Comp> normal_select(CriticalPairs<T, Comp>& critical_pairs) {
+    CriticalPairs<T, Comp> selected;
+    const auto min_degree = critical_pairs.GetSet().begin()->GetDegree();
+
+    for (auto it = critical_pairs.GetSet().begin(); it != critical_pairs.GetSet().end(); ) {
+        if (it->GetDegree() != min_degree) {
+            break;
+        }
+        selected.insert(*it);
+        it = critical_pairs.erase(it);
+    }
+    return selected;
+}
+
+template <typename T, typename Comp>
+static std::pair<PolynomialSet<T, Comp>, typename Polynomial<T, Comp>::container>
+sym_preproc(
+        const CriticalPairs<T, Comp>& critical_pairs,
+        const PolynomialSet<T, Comp>& poly_set) {
+    const T value_type_one(1);
+
+    PolynomialSet<T, Comp> to_reduce;
+    for (const auto& critical_pair : critical_pairs.GetSet()) {
+        to_reduce.AddPolynomial(critical_pair.leftPolynomial() *
+                                Term<T>(critical_pair.leftMonomial(), value_type_one));
+        to_reduce.AddPolynomial(critical_pair.rightPolynomial() *
+                                Term<T>(critical_pair.rightMonomial(), value_type_one));
+    }
+
+    typename Polynomial<T, Comp>::container relative_complement;
+    for (const auto& polynomial : to_reduce.PolSet()) {
+        for (const auto& term : polynomial.TermSet()) {
+            relative_complement.insert(Term<T>(term.monom(), value_type_one));
+        }
+    }
+
+    typename Polynomial<T, Comp>::container done_terms;
+    for (const auto& critical_pair : critical_pairs.GetSet()) {
+        Term<T> done_term(critical_pair.Getlcm(), value_type_one);
+        done_terms.insert(done_term);
+        relative_complement.erase(done_term);
+    }
+
+    while (!relative_complement.empty()) {
+        const Term<T> current_term = *relative_complement.begin();
+        done_terms.insert(current_term);
+        relative_complement.erase(relative_complement.begin());
+
+        for (const auto& polynomial : poly_set.PolSet()) {
+            if (!current_term.IsDivisibleBy(polynomial.LeadTerm())) {
+                continue;
+            }
+
+            const auto polynomial_to_add = polynomial * (current_term / polynomial.LeadTerm());
+            to_reduce.AddPolynomial(polynomial_to_add);
+
+            for (const auto& term : polynomial_to_add.TermSet()) {
+                Term<T> term_to_add(term.monom(), value_type_one);
+                if (done_terms.find(term_to_add) == done_terms.end()) {
+                    relative_complement.insert(term_to_add);
+                }
+            }
+            break;
+        }
+    }
+
+    return {to_reduce, done_terms};
+}
+
+template <typename T, typename Comp>
+static PolynomialSet<T, Comp> reduction(
+        const CriticalPairs<T, Comp>& critical_pairs,
+        const PolynomialSet<T, Comp>& poly_set) {
+    auto [results, all_terms] = sym_preproc(critical_pairs, poly_set);
+
+    const T value_type_one(1);
+    Polynomial<T, Comp> lead_terms;
+    for (const auto& polynomial : results.PolSet()) {
+        Term<T> term_to_add(polynomial.LeadTerm().monom(), value_type_one);
+        if (lead_terms.TermSet().find(term_to_add) == lead_terms.TermSet().end()) {
+            lead_terms += term_to_add;
+        }
+    }
+
+    return matrix_reduction(results, all_terms, lead_terms);
+}
+
+template <typename T, typename Comp, typename SelFunction>
+PolynomialSet<T, Comp>& calculated_fast_gb(
+        PolynomialSet<T, Comp>& groebner_basis,
+        SelFunction select_function) {
+    CriticalPairs<T, Comp> critical_pairs(groebner_basis);
+
+    while (!critical_pairs.empty()) {
+        const auto selected_set = select_function(critical_pairs);
+        const auto reduced_set = reduction(selected_set, groebner_basis);
+        for (const auto& res : reduced_set.PolSet()) {
+            for (const auto& basis_polynomial : groebner_basis.PolSet()) {
+                critical_pairs.insert(CriticalPair(res, basis_polynomial));
+            }
+            groebner_basis.AddPolynomial(res);
+        }
+    }
+    return groebner_basis;
+}
+
+template <typename T, typename Comp, typename SelFunction>
+PolynomialSet<T, Comp> calculate_fast_gb(
+        const PolynomialSet<T, Comp>& poly_set,
+        SelFunction select_function) {
+    auto groebner_basis = poly_set;
+    return calculated_fast_gb(groebner_basis, select_function);
+}
+
+}  // namespace gb
---
+++ GroebnerLib/includes/MatrixReduction.h
@@ -0,0 +1,255 @@
+#pragma once
+
+#include "PolynomialSet.h"
+
+namespace gb {
+
+template <typename T, typename Comp>
+PolynomialSet<T, Comp>
+matrix_reduction(
+    const PolynomialSet<T, Comp>&,  // Polynomials to reduce.
+    const typename Polynomial<T, Comp>::container&,  // Set of all terms.
+    const Polynomial<T, Comp>&  // Set of all lead terms.
+);
+
+// #define VECTOR_MAP
+
+#ifdef VECTOR_MAP
+
+/* Declaration */
+
+template <typename T>
+static size_t column_unzero(const std::vector<std::map<size_t, T>>&, size_t, size_t);
+
+template <typename T>
+static void triangulation(std::vector<std::map<size_t, T>>& matrix);
+
+/* Realization */
+
+template <typename T>
+static size_t column_unzero(
+        const std::vector<std::map<size_t, T>>& matrix,
+        size_t curren_line,
+        size_t current_column) {
+    const T value_type_zero(0);
+    for (size_t i = curren_line; i != matrix.size(); ++i) {
+        if (matrix[i].find(current_column) != matrix[i].end()) {
+           return i;
+        }
+    }
+    return matrix.size();
+}
+
+template <typename T>
+static void triangulation(
+        std::vector<std::map<size_t, T>>& matrix,
+        size_t column_count) {
+    const T value_type_zero(0), value_type_one(1);
+    size_t current_column = 0;
+    for (size_t i = 0; i != matrix.size() && current_column != column_count; ++current_column) {
+        size_t current_index = column_unzero(matrix, i, current_column);
+        if (current_index == matrix.size()) {
+            continue;
+        }
+
+        if (current_index != i) {
+            matrix[i].swap(matrix[current_index]);
+        }
+
+        if (matrix[i][current_column] != value_type_one) {
+            const T divider = matrix[i][current_column];
+            for (auto& [index, coefficient] : matrix[i]) {
+                coefficient /= divider;
+            }
+        }
+
+        for (size_t j = 0; j != matrix.size(); ++j) {
+            if (j == i) {
+                continue;
+            }
+            const auto it = matrix[j].find(current_column);
+            if (it == matrix[j].end()) {
+                continue;
+            }
+
+            const T factor = it->second;
+            for (const auto& [index, coefficient] : matrix[i]) {
+                const T res = matrix[j][index] - coefficient * factor;
+                if (res == value_type_zero) {
+                    matrix[j].erase(index);
+                } else {
+                    matrix[j][index] = res;
+                }
+            }
+        }
+        ++i;
+    }
+}
+
+template <typename T, typename Comp>
+PolynomialSet<T, Comp> matrix_reduction(
+        const PolynomialSet<T, Comp>& results,
+        const typename Polynomial<T, Comp>::container& all_terms,
+        const Polynomial<T, Comp>& lead_terms) {
+    std::vector<std::map<size_t, T>> matrix(results.PolSet().size());
+    size_t index = matrix.size() - 1;
+    for (auto it = results.PolSet().begin(); it != results.PolSet().end(); ++it, --index) {
+        size_t jndex = 0;
+        auto poly_it = it->TermSet().begin();
+        for (auto jt = all_terms.begin(); poly_it != it->TermSet().end(); ++jt, ++jndex) {
+            if (poly_it->monom() == jt->monom()) {
+                matrix[index][jndex] = poly_it->coefficient();
+                ++poly_it;
+            }
+        }
+    }
+
+    triangulation(matrix, all_terms.size());
+
+    const T value_type_zero(0), value_type_one(1);
+    PolynomialSet<T, Comp> reduced_results;
+    for (const auto& row : matrix) {
+        if (row.empty()) {
+            break;
+        }
+        typename Polynomial<T, Comp>::container polynomial;
+        auto it = all_terms.begin();
+        bool is_first = true;
+        size_t last_index = 0;
+        for (const auto& [index, coefficient] : row) {
+            std::advance(it, index - last_index);
+            last_index = index;
+            if (is_first) {
+                if (lead_terms.TermSet().find(Term<T>(it->monom(), value_type_one)) !=
+                        lead_terms.TermSet().end()) {
+                    break;
+                }
+                is_first = false;
+            }
+            polynomial.insert(Term<T>(it->monom(), coefficient));
+        }
+        reduced_results.AddPolynomial(polynomial);
+    }
+
+    return reduced_results;
+}
+
+#else
+
+/* Declaration */
+
+template <typename T>
+static size_t column_unzero(const std::vector<std::vector<size_t>>&, size_t);
+
+template <typename T>
+static void triangulation(std::vector<std::vector<T>>& matrix);
+
+/* Realization */
+
+template <typename T>
+size_t column_unzero(
+        const std::vector<std::vector<T>>& matrix,
+        size_t curren_line,
+        size_t current_column) {
+    const T value_type_zero(0);
+    for (size_t i = curren_line; i != matrix.size(); ++i) {
+        if (matrix[i][current_column] != value_type_zero) {
+           return i;
+        }
+    }
+    return matrix.size();
+}
+
+template <typename T>
+void triangulation(std::vector<std::vector<T>>& matrix) {
+    const T value_type_zero(0), value_type_one(1);
+    size_t current_column = 0;
+    for (size_t i = 0; i != matrix.size() && current_column != matrix.front().size(); ++current_column) {
+        size_t current_index = column_unzero(matrix, i, current_column);
+        if (current_index == matrix.size()) {
+            continue;
+        }
+
+        if (current_index != i) {
+            matrix[i].swap(matrix[current_index]);
+        }
+
+        if (matrix[i][current_column] != value_type_one) {
+            const T factor = matrix[i][current_column];
+            for (size_t j = current_column; j != matrix[i].size(); ++j) {
+                matrix[i][j] /= factor;
+            }
+        }
+
+        for (size_t j = 0; j != matrix.size(); ++j) {
+            if (j == i) {
+                continue;
+            }
+            if (matrix[j][current_column] != value_type_zero) {
+                const T factor = matrix[j][current_column];
+                for (size_t k = current_column; k != matrix[j].size(); ++k) {
+                    matrix[j][k] -= matrix[i][k] * factor;
+                }
+            }
+        }
+        ++i;
+    }
+}
+
+template <typename T, typename Comp>
+PolynomialSet<T, Comp> matrix_reduction (
+        const PolynomialSet<T, Comp>& results,
+        const typename Polynomial<T, Comp>::container& all_terms,
+        const Polynomial<T, Comp>& lead_terms) {
+    std::vector<std::vector<T>> matrix(results.PolSet().size(), std::vector<T>(all_terms.size()));
+
+    size_t index = matrix.size() - 1;
+    for (auto it = results.PolSet().begin(); it != results.PolSet().end(); ++it, --index) {
+        size_t jndex = 0;
+        auto poly_it = it->TermSet().begin();
+        for (auto jt = all_terms.begin(); poly_it != it->TermSet().end(); ++jt, ++jndex) {
+            if (poly_it->monom() == jt->monom()) {
+                matrix[index][jndex] = poly_it->coefficient();
+                ++poly_it;
+            }
+        }
+    }
+
+    triangulation(matrix);
+
+    const T value_type_zero(0), value_type_one(1);
+    PolynomialSet<T, Comp> reduced_results;
+    for (const auto& row : matrix) {
+        typename Polynomial<T, Comp>::container polynomial;
+        auto it = all_terms.begin();
+        bool was_first = false, is_all_zeros = true;
+        for (const auto& coefficient : row) {
+            if (coefficient == value_type_zero) {
+                ++it;
+                continue;
+            }
+            is_all_zeros = false;
+
+            if (!was_first) {
+                if (lead_terms.TermSet().find(Term<T>(it->monom(), value_type_one)) !=
+                        lead_terms.TermSet().end()) {
+                    break;
+                }
+                was_first = true;
+            }
+
+            polynomial.insert(Term<T>(it->monom(), coefficient));
+            ++it;
+        }
+        if (is_all_zeros) {
+            break;
+        }
+        reduced_results.AddPolynomial(polynomial);
+    }
+
+    return reduced_results;
+}
+
+#endif
+
+}  // namespace gb
