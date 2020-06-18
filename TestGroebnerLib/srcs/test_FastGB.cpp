a6d0f240dc0f1d04cf46333d058b51eb78c4b77e
cdraugr- <kvm2014@bk.ru>
2020-06-18 11:14:27 +0300

    F4 tests added

--- TestGroebnerLib/srcs/test_FastGB.cpp
+++ TestGroebnerLib/srcs/test_FastGB.cpp
@@ -1,5 +1,37 @@
 #include "Tests.h"
 
-void test_FastGB() {
+static void test_FastGBTime(gb::PolynomialSet<Rational, gb::LexComp>& ideal) {
+    gb::calculated_fast_gb(ideal);
+}
 
+void test_FastGB(u32 maximum_variables) {
+    gb::Polynomial<Rational> i1({  // HW 07, ex 01
+        gb::Term<Rational>(gb::Monomial({2, 1, 0}), 1),
+        gb::Term<Rational>(gb::Monomial({1, 0, 1}), -1),
+    });
+    gb::Polynomial<Rational> i2({
+        gb::Term<Rational>(gb::Monomial({0, 2, 0}), 1),
+        gb::Term<Rational>(gb::Monomial({0, 1, 1}), 2),
+    });
+    gb::PolynomialSet<Rational> ideal({i1, i2});
+
+    std::cout << "Before F4 {\n" << ideal << "\n}\n";
+    gb::calculated_fast_gb(ideal);
+    std::cout << "After F4  {\n" << ideal.ReduceBasis() << "\n}\n\n";
+    assert(gb::IsGroebnerBasis(ideal) == true);
+
+    for (u32 i = 1; i <= maximum_variables ; ++i) {
+        auto ideal = gb::GiveSigmaSet<Rational, gb::LexComp>(i);
+
+        std::string text = "Time spent for calculate Cyclic ";
+        text.push_back('0' + i);
+        test_time(test_FastGBTime, text + " Groebner Basis is ")(ideal);
+
+        if (gb::IsGroebnerBasis(ideal.ReduceBasis()) != true) {
+            std::cout << "F4 Tests status: \033[1;31mFAIL\033[0m.\n\n";
+            return;
+        }
+    }
+
+    std::cout << "\nF4 Tests status: \033[1;32mOK\033[0m.\n";
 }
